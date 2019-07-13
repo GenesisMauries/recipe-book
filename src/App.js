4c36d20fb73e2aa3ae7aac5147a2c634ebf31c80
GenesisMauries <exodo_54@hotmail.com>
2019-07-13 17:51:46 -0500

    Sube componente de visualizacion de data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, {Component} from 'react';
-import Searcher from './components/searcher/Searcher';
 import './App.css';
+import Searcher from './components/searcher/Searcher';
+import Recipes from './components/recipes/Recipes'
+
 
 const key = "d8c18edd0b7973738f811b593d8ad815";
 
 class App extends Component{
+  state = {
+    recipes:[]
+  }
   getData = async (e)=>{
     const search= e.target.elements.search.value
-    console.log(search)
+    // console.log(search)
     e.preventDefault()
     const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${key}&q=${search}&count=15`)
     const data = await api_call.json()
-    console.log(data)
-    
+    // console.log(data)
+    this.setState({recipes:data.recipes})
+    console.log(this.state.recipes)
   }
   render(){
     return (
@@ -21,6 +27,7 @@ class App extends Component{
             <h1 className="App-title">Recipe book</h1>
           </header>
        <Searcher getData={this.getData}/>
+       <Recipes recipes={this.state.recipes}/>
       </div>
     );
   }
