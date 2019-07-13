5ba1de0d7055f7804310bab7e15c46fc6c8d0577
GenesisMauries <exodo_54@hotmail.com>
2019-07-13 16:07:48 -0500

    Updates

---
+++ precache-manifest.7f5b283bd7410a6a582879673f65287a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f812af0a97d5de5ddbbc5b15ea3112f8",
+    "url": "/recipe-book/index.html"
+  },
+  {
+    "revision": "07dbc5d174644476d731",
+    "url": "/recipe-book/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/recipe-book/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "07dbc5d174644476d731",
+    "url": "/recipe-book/static/js/main.c9b6eea5.chunk.js"
+  },
+  {
+    "revision": "3b717f7a139da6ad6739",
+    "url": "/recipe-book/static/js/runtime~main.5d8d1fe1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipe-book/static/media/logo.5d5d9eef.svg"
+  }
+]);
