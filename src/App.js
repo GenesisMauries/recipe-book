4f823e3f5ca980afea88ae3f3369363adc0cb896
GenesisMauries <exodo_54@hotmail.com>
2019-07-13 16:42:27 -0500

    Sube buscador y resultados en consola

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Searcher from './components/searcher/Searcher';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const key = "d8c18edd0b7973738f811b593d8ad815";
+
+class App extends Component{
+  getData = async (e)=>{
+    const search= e.target.elements.search.value
+    console.log(search)
+    e.preventDefault()
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${key}&q=${search}&count=15`)
+    const data = await api_call.json()
+    console.log(data)
+    
+  }
+  render(){
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <h1 className="App-title">Recipe book</h1>
+          </header>
+       <Searcher getData={this.getData}/>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
